72f5ee65d633a0bbdad90f2a650a88efbfccc67c
Michał Szyszka <mszyszka223@gmail.com>
2017-09-11 15:20:37 +0200

    in progress

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
-function show_menu(){
-    document.getElementById("container-ol").style.display="block";
-}
+//function show_menu(){
+//    //there is a [0] before .style, becouse document.getElementByClassName delivery object that is NodeList. We need to point one element from this object
+//    document.getElementsByClassName("mens-container-ol")[0].style.display="block";
+//}
+

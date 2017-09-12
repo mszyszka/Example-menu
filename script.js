63bc271cf9cb240eafbf4646090806eb639bee58
Michał Szyszka <mszyszka223@gmail.com>
2017-09-12 13:23:19 +0200

    Almost done

--- script.js
+++
@@ -1,6 +0,0 @@
-
-//function show_menu(){
-//    //there is a [0] before .style, becouse document.getElementByClassName delivery object that is NodeList. We need to point one element from this object
-//    document.getElementsByClassName("mens-container-ol")[0].style.display="block";
-//}
-
